1823caea41d42741c134c395b6277aa01d2b05cc
VICTOR M OTTATI <victor.ottati@gmail.com>
2023-05-02 16:30:08 +0100

    UPDATE: HornedBeast.js & HornedBeast.css

--- src/components/HornedBeast/HornedBeast.js
+++ src/components/HornedBeast/HornedBeast.js
@@ -9,22 +9,19 @@ export const HornedBeast = ({
 	keyword,
 	description,
 }) => {
-	// console.log(data);
 	return (
-		<div className="beasts-container">
-			<div key={id} className="beast">
-				<h3>{title}</h3>
-				<p>
-					Horns: <span>{`${horns}`} </span>
-				</p>
-				<p>
-					Keyword: <span>{`${keyword}`} </span>
-				</p>
-				<img src={image_url} alt={title} />
-				<p className="description">
-					<span>{description}.</span>
-				</p>
-			</div>
+		<div key={id} className="beast">
+			<h3>{title}</h3>
+			<p>
+				Horns: <span>{`${horns}`} </span>
+			</p>
+			<p>
+				Keyword: <span>{`${keyword}`} </span>
+			</p>
+			<img src={image_url} alt={title} />
+			<p className="description">
+				<span>{description}.</span>
+			</p>
 		</div>
 	);
 };
